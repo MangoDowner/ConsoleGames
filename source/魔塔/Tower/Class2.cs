cf1d35b3d804dce62e0c18490b7f4cfb7133f41e
MangoDowner <353988117@qq.com>
2020-10-06 17:56:42 +0800

    Update Class2.cs

--- source/魔塔/Tower/Class2.cs
+++ source/魔塔/Tower/Class2.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace Tower
 {
     public enum ConsoleKey2
@@ -17,30 +18,26 @@ namespace Tower
         Up,
         Down,
     }
-
 }
 
 
 namespace Tower
 {
-
-
-
-    public class Container//容器  封装怪物 装备 npc 地图
+    public class Container //容器  封装怪物 装备 npc 地图
     {
-        public Dictionary<int, Monster> ms = new Dictionary<int, Monster>();//怪物
-        public Dictionary<int, Npc> npc = new Dictionary<int, Npc>();//Npc
-        public Dictionary<int, Prop> eq = new Dictionary<int, Prop>();//道具
+        public Dictionary<int, Monster> ms = new Dictionary<int, Monster>(); //怪物
+        public Dictionary<int, Npc> npc = new Dictionary<int, Npc>(); //Npc
+        public Dictionary<int, Prop> eq = new Dictionary<int, Prop>(); //道具
         public Map map1s;
-        public Dictionary<int, Stairs> stair = new Dictionary<int, Stairs>();//楼梯
-        public Dictionary<int, Gate> gate = new Dictionary<int, Gate>();//门
-        public Dictionary<int, Key> key = new Dictionary<int, Key>();//钥匙
-        public Dictionary<int, Wall> wall = new Dictionary<int, Wall>();//墙壁
-        public Dictionary<int, Func_Prop> func = new Dictionary<int, Func_Prop>();//特殊功能性道具
-        public int Up_x;
-        public int Up_y;
-        public int Down_x;
-        public int Down_y;
+        public Dictionary<int, Stairs> stair = new Dictionary<int, Stairs>(); //楼梯
+        public Dictionary<int, Gate> gate = new Dictionary<int, Gate>(); //门
+        public Dictionary<int, Key> key = new Dictionary<int, Key>(); //钥匙
+        public Dictionary<int, Wall> wall = new Dictionary<int, Wall>(); //墙壁
+        public Dictionary<int, Func_Prop> func = new Dictionary<int, Func_Prop>(); //特殊功能性道具
+        public int UpX;
+        public int UpY;
+        public int DownX;
+        public int DownLeft;
     }
 
     public class Play //玩家
@@ -59,10 +56,9 @@ namespace Tower
         public int RedKey = 0;
         public bool Jump = false;
         public bool See = false;
-
     }
 
-    public class Monster//怪物
+    public class Monster //怪物
     {
         public string Name;
         public int Hp;
@@ -71,6 +67,7 @@ namespace Tower
         public int Level;
         public int x;
         public int y;
+
         public int Slime(int _x, int _y)
         {
             Name = "史莱姆";
@@ -82,6 +79,7 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
+
         public int Skeleton(int _x, int _y)
         {
             Name = "骷髅怪";
@@ -93,6 +91,7 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
+
         public int Skeleton2(int _x, int _y)
         {
             Name = "骷髅将军";
@@ -104,6 +103,7 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
+
         public int Bat(int _x, int _y)
         {
             Name = "小蝙蝠";
@@ -115,6 +115,7 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
+
         public int Master(int _x, int _y)
         {
             Name = "暗黑法师";
@@ -126,6 +127,7 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
+
         public int Giant(int _x, int _y)
         {
             Name = "巨人";
@@ -137,6 +139,7 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
+
         public int Devil(int _x, int _y)
         {
             Name = "魔王";
@@ -158,6 +161,7 @@ namespace Tower
                 {
                     c = false;
                 }
+
                 int x, y;
                 x = play.Atk - Dfs;
                 y = Atk - play.Dfs;
@@ -165,16 +169,19 @@ namespace Tower
                 {
                     x = 0;
                 }
+
                 if (y < 0)
                 {
                     y = 0;
                 }
+
                 play.Hp = play.Hp - y;
                 Hp = Hp - x;
 
                 //Console.ReadKey();
                 // 信息
             }
+
             if (Hp < 0)
             {
                 return true;
@@ -183,10 +190,11 @@ namespace Tower
             {
                 return false;
             }
+
             return true;
         }
 
-        public int Battle2(int x,int y,int z)//x指攻击，y防御，z血量！
+        public int Battle2(int x, int y, int z) //x指攻击，y防御，z血量！
         {
             int a, s, d;
             a = Hp;
@@ -196,40 +204,43 @@ namespace Tower
             bool c = true;
             while (c)
             {
-                if (z<0||a<0)
+                if (z < 0 || a < 0)
                 {
                     c = false;
                 }
+
                 int tmp_x, tmp_y;
                 tmp_x = x - d;
                 tmp_y = s - y;
-                if (tmp_x<0)
+                if (tmp_x < 0)
                 {
                     tmp_x = 0;
                 }
-                if (tmp_y<0)
+
+                if (tmp_y < 0)
                 {
                     tmp_y = 0;
                 }
+
                 z = z - tmp_y;
                 a = a - tmp_x;
-
             }
-            if (z<0)
+
+            if (z < 0)
             {
                 return 9999;
             }
 
-            if (a<0)
+            if (a < 0)
             {
                 return Tmp_z - z;
             }
-            return 0;
 
+            return 0;
         }
     }
 
-    public class Prop//道具
+    public class Prop //道具
     {
         public string Name;
         public int Addatk;
@@ -237,7 +248,8 @@ namespace Tower
         public int AddHp;
         public int x;
         public int y;
-        public int Sword(int _x, int _y)//圣剑
+
+        public int Sword(int _x, int _y) //圣剑
         {
             Name = "圣剑";
             Addatk = 50;
@@ -246,7 +258,8 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
-        public int Shield(int _x, int _y)//圣盾
+
+        public int Shield(int _x, int _y) //圣盾
         {
             Name = "圣盾";
             Addatk = 0;
@@ -255,7 +268,8 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
-        public int PotionRed(int _x, int _y)//红血瓶
+
+        public int PotionRed(int _x, int _y) //红血瓶
         {
             Name = "红血瓶";
             AddHp = 50;
@@ -263,7 +277,8 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
-        public int PotionBlue(int _x, int _y)//蓝血瓶
+
+        public int PotionBlue(int _x, int _y) //蓝血瓶
         {
             Name = "蓝血瓶";
             AddHp = 100;
@@ -271,7 +286,8 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
-        public int GemstoneBlue(int _x, int _y)//蓝宝石
+
+        public int GemstoneBlue(int _x, int _y) //蓝宝石
         {
             Name = "蓝宝石";
             Adddfs = 5;
@@ -279,7 +295,8 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
-        public int GemstoneRed(int _x, int _y)//红宝石
+
+        public int GemstoneRed(int _x, int _y) //红宝石
         {
             Name = "红宝石";
             Addatk = 5;
@@ -289,12 +306,12 @@ namespace Tower
         }
     }
 
-    public class Map//地图
+    public class Map //地图
     {
         public char[,] maps = new char[13, 13];
         public ConsoleColor[,] color_buffer = new ConsoleColor[13, 13];
 
-        public void Fill_Map()//地图填充
+        public void Fill_Map() //地图填充
         {
             for (int i = 0; i < 13; i++)
             {
@@ -304,7 +321,8 @@ namespace Tower
                 }
             }
         }
-        public void Fill_Map2()//地图颜色填充
+
+        public void Fill_Map2() //地图颜色填充
         {
             for (int i = 0; i < 13; i++)
             {
@@ -315,7 +333,7 @@ namespace Tower
             }
         }
 
-        public void Boundary_Map()//地图边界填充
+        public void Boundary_Map() //地图边界填充
         {
             for (int i = 0; i < 13; i++)
             {
@@ -324,9 +342,9 @@ namespace Tower
                 maps[12, i] = '█';
                 maps[i, 12] = '█';
             }
-
         }
-        public void Print()//打印地图
+
+        public void Print() //打印地图
         {
             for (int i = 0; i < 13; i++)
             {
@@ -337,35 +355,35 @@ namespace Tower
                         Console.WriteLine(maps[i, j]);
                         break;
                     }
+
                     if (maps[i, j] < 128)
                     {
-
-
                         Console.Write(maps[i, j]);
                         Console.Write(" ");
                         continue;
                     }
+
                     Console.Write(maps[i, j]);
                 }
             }
         }
-
     }
 
-    public class Npc//Npc
+    public class Npc //Npc
     {
         public String Name;
         public String Dialogue;
         public int x;
         public int y;
 
-        public int Devil_Shop(int _x,int _y)
+        public int Devil_Shop(int _x, int _y)
         {
             Name = "恶魔商店";
             x = _x;
             y = _y;
             return Program.MapPos(x, y);
         }
+
         public int Exp_Shop(int _x, int _y)
         {
             Name = "经验商人";
@@ -373,6 +391,7 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
+
         public int Money_Shop(int _x, int _y)
         {
             Name = "钥匙商人";
@@ -380,6 +399,7 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
+
         public int Oldman1(int _x, int _y)
         {
             Name = "老人";
@@ -388,6 +408,7 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
+
         public int Oldman2(int _x, int _y)
         {
             Name = "富商";
@@ -396,6 +417,7 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
+
         public int Oldman3(int _x, int _y)
         {
             Name = "祭师";
@@ -404,14 +426,14 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
-
     }
 
-    public class Stairs//楼梯
+    public class Stairs //楼梯
     {
         public Dict type;
         public int x;
         public int y;
+
         public int TypeStairs(Dict s, int _x, int _y)
         {
             type = s;
@@ -421,7 +443,7 @@ namespace Tower
         }
     }
 
-    public class Gate//门
+    public class Gate //门
     {
         public ConsoleKey2 type;
         public int x;
@@ -434,10 +456,9 @@ namespace Tower
             y = _y;
             return Program.MapPos(x, y);
         }
-
     }
 
-    public class Key//钥匙
+    public class Key //钥匙
     {
         public ConsoleKey2 type;
         public int x;
@@ -452,32 +473,33 @@ namespace Tower
         }
     }
 
-    public class Wall//墙壁
+    public class Wall //墙壁
     {
         public int x;
         public int y;
+
         public int Wallpos(int _x, int _y)
         {
-
             x = _x;
             y = _y;
             return Program.MapPos(x, y);
         }
     }
 
-    public class Func_Prop//功能道具
+    public class Func_Prop //功能道具
     {
         public String Name;
         public int x;
         public int y;
 
-        public int Jump_Prop(int _x,int _y)
+        public int Jump_Prop(int _x, int _y)
         {
             Name = "楼层跳跃魔杖";
             x = _x;
             y = _y;
-            return Program.MapPos(x,y);
+            return Program.MapPos(x, y);
         }
+
         public int Memory_Prop(int _x, int _y)
         {
             Name = "怪物字典";
